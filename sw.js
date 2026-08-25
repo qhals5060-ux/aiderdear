9c4c75cb73a4711ad938fbccbc9d4d0199dd0d63
qhals5060-ux <qhals5060@gmail.com>
2026-08-25 22:13:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aiderdear-v41-firebase-couples';
+const CACHE='aiderdear-v42-routine-layout';
 const APP_SHELL=['./','./index.html','./firebase-app.js','./manifest.webmanifest','./aiderdear-icon.svg','./aiderdear-icon-180.png','./aiderdear-icon-192.png','./aiderdear-icon-512.png','./aiderdear-sky.jpg'];
 
 self.addEventListener('install',event=>{
