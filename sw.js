37b0d506a69575eb2f9e5cc5dbb866957382c190
qhals5060-ux <qhals5060@gmail.com>
2026-08-25 18:24:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aiderdear-v31-schedule-responsive';
+const CACHE='aiderdear-v30-routine-dashboard';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./aiderdear-icon.svg','./aiderdear-sky.jpg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
